e07fa9bf29fc7c82431fd6e994f78ae605d17c4d
shosetim <shosetim32@gmail.com>
2022-11-07 14:33:01 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './styles/main.scss';
+import example from './images/example.jpg';
+
+console.log(example);
+// Create a class property without a constructor
+class Game {
+  name = 'Violin Charades'
+}
+const myGame = new Game()
+// Create paragraph node
+const p = document.createElement('p')
+
+console.log(p);
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const postcssPresetEnv = require('postcss-preset-env');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].[contenthash].bundle.js',
+    assetModuleFilename: 'images/[name][ext]',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      template: path.resolve(__dirname, './src/template.html'), // template file
+      filename: 'index.html', // output file
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  postcssPresetEnv({ browsers: 'last 2 versions' })
+                ]
+              }
+
+            }
+          },
+          'sass-loader'
+        ],
+      },
+    ]
+  }
+}
